619277e8bbbfd6ee3bdd9ebe336722620390fb8e
Kai Stevens <kai.stevens97@gmail.com>
2021-12-24 14:51:54 +0000

    created the basic math functions and linked it an operator function

---
+++ scripts.js
@@ -0,0 +1,44 @@
+// Functions for basic math operations
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+
+    // check for dividing by zero
+    if(b == 0){
+        return 'Don\'t divide by zero mate';
+    }
+    // Round to two decimal places
+    return  (a / b).toFixed(2);
+}
+
+// Carry out the operation, based on the given operator (a string)
+function operate(operator, num1, num2){
+    if(operator == '+'){
+        return add(num1, num2);
+    }
+
+    else if(operator == '-'){
+        return subtract(num1, num2);
+    }
+
+    else if(operator == '*'){
+        return multiply(num1, num2);
+    }
+
+    else if(operator == '/'){
+        return divide(num1, num2);
+    }
+}
+
+console.log(operate('*', 2, 5));
