7d06b400d13db2bb964bbbe67b993bf5f5c7905e
Kai Stevens <kai.stevens97@gmail.com>
2021-12-27 17:16:45 +0000

    added the basic visual setup for the calculators display and the button area

--- scripts.js
+++ scripts.js
@@ -15,7 +15,7 @@ function multiply(a, b){
 function divide(a, b){
 
     // check for dividing by zero
-    if(b == 0){
+    if(b === 0){
         return 'Don\'t divide by zero mate';
     }
     // Round to two decimal places
@@ -24,21 +24,22 @@ function divide(a, b){
 
 // Carry out the operation, based on the given operator (a string)
 function operate(operator, num1, num2){
-    if(operator == '+'){
+    if(operator === '+'){
         return add(num1, num2);
     }
 
-    else if(operator == '-'){
+    else if(operator === '-'){
         return subtract(num1, num2);
     }
 
-    else if(operator == '*'){
+    else if(operator === '*'){
         return multiply(num1, num2);
     }
 
-    else if(operator == '/'){
+    else if(operator === '/'){
         return divide(num1, num2);
     }
 }
 
+// the functions
 console.log(operate('*', 2, 5));
